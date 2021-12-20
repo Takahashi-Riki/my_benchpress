efca78914972aa548d06a794dff867aabd57252c
Takahashi-Riki <riki.riki0218@gmail.com>
2021-12-20 11:41:07 +0900

    make it possible to change the order of microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,4 @@ class Micropost < ApplicationRecord
   validates :time, presence: true
   validates :comment, length: { maximum: 100 }
   validates :order, inclusion: { in: (1..100)}
-  default_scope -> { order(created_at: :asc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get "/login" => "session#new"
   post "/login" => "session#create"
   delete "/logout" => "session#destroy"
-  resources :microposts, only: [:index, :create, :new, :destroy]
+  resources :microposts, only: [:index, :create, :new, :destroy] do
+    member do
+      patch "/:direction" => "microposts#update"
+    end
+  end
 end
